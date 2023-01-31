58e6d38ec99eea05064a5bfea0f3b086909fcb97
juanpesca19 <juanpesca19@DESKTOP-278S0F8>
2023-01-31 20:04:11 +0100

    ej 1 y 2, 3 queda funcion

---
+++ Tema5EjerciciosArraysFunciones/src/ejercicio1/Main.java
@@ -0,0 +1,13 @@
+package ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int tabla [] = {2,4,6,8,10};
+		
+		System.out.println(Funcion.sumaPosiciones(tabla));
+
+	}
+
+}
