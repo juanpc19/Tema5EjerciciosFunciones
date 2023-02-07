880c8d8654fc18c6c1bf1c6ffdb2d8fcca912bac
juanpc19 <juan.perez@iesnervion.es>
2023-02-07 14:18:11 +0100

    posible con 2 for

--- Tema5EjerciciosArraysFunciones/src/ejercicio10/FuncionGira90.java
+++ Tema5EjerciciosArraysFunciones/src/ejercicio10/FuncionGira90.java
@@ -10,10 +10,11 @@ public class FuncionGira90 {
 
 		for (int fila = 0; fila < tablaInicio.length; fila++) {
 			
-			tablaCopia=Arrays.copyOf(tablaCopia,tablaCopia.length+1);
 			tablaCopia=Arrays.copyOf(tablaCopia,tablaCopia[0].length+1);
 			
 			for (int columna = 0; columna >tablaInicio.length ; columna++) {
+				
+				tablaCopia=Arrays.copyOf(tablaCopia,tablaCopia.length+1);
 				//tablaInicio[fila][columna]
 				//tablaCopia[fila][columna] = tablaInicio[columna][fila];
 				
